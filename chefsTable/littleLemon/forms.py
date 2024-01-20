b47dbb43f8ae44bbc40cfeb44737968be9968bf9
sabbasachiPH <rentustat02@gmail.comm>
2024-01-20 15:55:23 +0600

    form_model branch second commit

--- chefsTable/littleLemon/forms.py
+++ chefsTable/littleLemon/forms.py
@@ -15,7 +15,6 @@ SHIFT = [
 class InputForm(forms.Form): 	
 	first_name = forms.CharField(max_length = 200) 
 	last_name = forms.CharField(max_length = 200) 
-	shift = forms.ChoiceField(choices = SHIFT)
 	time_log = forms.TimeField(help_text="Enter the exact time")
  
 	
